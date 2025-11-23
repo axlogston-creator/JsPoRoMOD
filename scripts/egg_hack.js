13366e8cd10971a25656c4f12b186cd575423b14
axlogston-creator <axl.ogston@kds.vic.edu.au>
2025-11-23 15:21:44 +1100

    Update egg_hack.js

--- scripts/egg_hack.js
+++ scripts/egg_hack.js
@@ -1,5 +1,5 @@
-let tier = 3;  // Tier of Egg (0: Common, 1: Rare, 2: Epic, 3: Legendary)
-let gachaType = 1;  // Gacha type of eggs: (0: Move, 1: Legendary, 2: Shiny)
+let tier = 3;  // Tier of Egg (0: Legendary, 1: Rare, 2: Epic, 3: Common)
+let gachaType = 1;  // Gacha type of eggs: (0: Move, 2: Legendary, 1: Shiny)
 let hatchWaves = 0;  // Hatch eggs after this amount of time.
 
 Phaser.Display.Canvas.CanvasPool.pool[0].parent.game.scene.keys.battle.gameData.eggs.forEach(
