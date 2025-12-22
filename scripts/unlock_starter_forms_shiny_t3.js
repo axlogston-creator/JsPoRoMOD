ed7ea3fe0f0a13a6e64d3b8ada8f41037375b957
axlogston-creator <axl.ogston@kds.vic.edu.au>
2025-12-22 16:26:23 +1100

    Update unlock_starter_forms_shiny_t3.js

--- scripts/unlock_starter_forms_shiny_t3.js
+++ scripts/unlock_starter_forms_shiny_t3.js
@@ -1,4 +1,4 @@
-// Unlocks all forms for all starter pokemon and sets them all to shiny t3
+ Unlocks all forms for all starter pokemon and sets them all to shiny t3
 const dexData = Phaser.Display.Canvas.CanvasPool.pool[0].parent.game.scene.keys.battle.gameData.dexData;
 
 Object.keys(dexData).forEach(key => {
